f4e45519ea660a5931174b0ba0a4a271031947cf
Daria <darena-smol@yandex.ru>
2022-06-20 21:21:06 +0500

    12

---
+++ Zadach 15/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите номер дня: ");
+int numberDay = int.Parse(Console.ReadLine());
+if (numberDay == 1)
+{
+    Console.WriteLine($"{numberDay} день недели - это ПОНЕДЕЛЬНИК, рабочий день");
+}
+else if (numberDay == 2)
+{
+     Console.WriteLine($"{numberDay} день недели - это ВТОРНИК, рабочий день");
+}
+else if (numberDay == 3)
+{
+     Console.WriteLine($"{numberDay} день недели - это СРЕДА, маленькая пятница, но рабочий день");
+}
+else if (numberDay == 4)
+{
+     Console.WriteLine($"{numberDay} день недели - это ЧЕТВЕРГ, рабочий день");
+}
+else if (numberDay == 5)
+{
+     Console.WriteLine($"{numberDay} день недели - это ПЯТНИЦА, рабочий день, а завтра уже будет выходной");
+}
+else if (numberDay == 6)
+{
+     Console.WriteLine($"{numberDay} день недели - это СУББОТА, выходной день");
+}
+else if (numberDay == 7)
+{
+     Console.WriteLine($"{numberDay} день недели - это ВОСКРЕСЕНЬЕ, выходной день, а завтра на работу");
+}
+else
+{
+     Console.WriteLine($"{numberDay} - такого дня недели нет, в 1 неделе всего 7 дней");
+}
