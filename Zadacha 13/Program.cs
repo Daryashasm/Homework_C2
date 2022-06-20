74abf35d09521e77c3e284a23a63cd5a209068c1
Daria <darena-smol@yandex.ru>
2022-06-20 21:24:59 +0500

    13

---
+++ Zadacha 13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число; А = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+numberA = Math.Abs(numberA);
+
+if (numberA < 100)
+{
+     Console.WriteLine($"{numberA} - в этом числе нет третьей цифры");
+}
+else
+{
+    string numberB = numberA.ToString();
+    Console.WriteLine($"{numberB[2]} - это третья цифра введенного числа");
+}
